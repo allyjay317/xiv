8d6cfed5352da895047218bc4d5d003441b81a9b
Allison Usher <allyjay317@gmail.com>
2025-01-11 23:59:52 -0800

    add sqlx and organize workspace

--- backend/src/backend/server/go.mod
+++ backend/src/backend/server/go.mod
@@ -1,11 +1,15 @@
-module alyjay/xiv
+module github.com/alyjay/xiv
 
 go 1.23.4
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/jackc/pgx/v5 v5.7.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/alyjay/xivdb v0.0.0-00010101000000-000000000000
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
+
+require github.com/jmoiron/sqlx v1.4.0 // indirect
+
+replace github.com/alyjay/xivdb => ../database
--- backend/src/backend/server/user/user.go
+++ backend/src/backend/server/user/user.go
@@ -1,45 +1,28 @@
 package user
 
 import (
-	"database/sql"
 	"encoding/json"
-	"errors"
 	"net/http"
-	"os"
 
+	database "github.com/alyjay/xivdb"
 	"github.com/google/uuid"
 
 	_ "github.com/lib/pq"
 )
 
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-}
-
-func GetDb() (db *sql.DB, err error) {
-	connStr, exists := os.LookupEnv("DATABASE_CONN_STR")
-	if !exists {
-		return nil, errors.New("connection string Not Found")
-	}
-	db, err = sql.Open("postgres", connStr)
-	return db, err
+	ID       string `json:"id,omitempty" db:"id"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	var users []User
-	db, err := GetDb()
+	db, err := database.GetDb(w)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Database Connection Error"))
+		return
 	}
-	rows, err := db.Query("SELECT id, username, email FROM users")
-	for rows.Next() {
-		var u User
-		err = rows.Scan(&u.ID, &u.Username, &u.Email)
-		users = append(users, u)
-	}
+	err = db.Select(&users, "SELECT id, username, email FROM users")
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -54,22 +37,8 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&newUser)
 	newUUID := uuid.NewString()
 
-	db, err := GetDb()
+	db, err := database.GetDb(w)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Server Issue"))
-		return
-	}
-	userNameOrEmailExists := false
-	err = db.QueryRow(`SELECT EXISTS(SELECT 1 FROM users WHERE username = $1 OR email = $2) AS "exists"`, newUser.Username, newUser.Email).Scan(&userNameOrEmailExists)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Server Issue"))
-		return
-	}
-	if userNameOrEmailExists {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 User exists"))
 		return
 	}
 
