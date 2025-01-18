8ba1344ae4b6ffe4a45bc4f4f70300629e0a2cb6
Allison Usher <allyjay317@gmail.com>
2025-01-17 16:39:43 -0800

    Add gear-set table migrations

---
+++ backend/src/backend/database/migrations/000003_create_gear_set_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS gear_sets(
+    id UUID PRIMARY KEY,
+    user_id  UUID references users(id),
+    character_id varchar (50) references characters(character_id),
+    name VARCHAR (50),
+    config JSONB
+)
