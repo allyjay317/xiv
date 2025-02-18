8ae4b4c738e1cb1ad56c5ca522d3dffd9c5214e3
Allison Usher <allyjay317@gmail.com>
2025-02-17 21:10:28 -0800

    fix lint

--- frontend/src/Fixtures/GearSet.ts
+++ frontend/src/Fixtures/GearSet.ts
@@ -1,21 +1,24 @@
-import { GearSet, GearSource, Jobs, Slot } from '../utils/types'
+import { GearPiece, GearSet, GearSource, Jobs, Slot } from '../utils/types'
+
+const piece: Omit<GearPiece, 'source'> =  { augmented: false, have: false, priority: 0 }
 
 export const gearSet: GearSet = {
   id: '1',
   items: {
-    [Slot.BODY]: { augmented: false, have: false, source: GearSource.RAID },
-    [Slot.BRACELET]: { augmented: false, have: false, source: GearSource.RAID },
-    [Slot.EARRINGS]: { augmented: false, have: false, source: GearSource.TOME },
-    [Slot.FEET]: { augmented: false, have: false, source: GearSource.RAID },
-    [Slot.HANDS]: { augmented: false, have: false, source: GearSource.TOME },
-    [Slot.HEAD]: { augmented: false, have: false, source: GearSource.TOME },
-    [Slot.LEGS]: { augmented: false, have: false, source: GearSource.TOME },
-    [Slot.NECKLACE]: { augmented: false, have: false, source: GearSource.RAID },
-    [Slot.RING1]: { augmented: false, have: false, source: GearSource.TOME },
-    [Slot.RING2]: { augmented: false, have: false, source: GearSource.TOME },
-    [Slot.WEAPON]: { augmented: false, have: false, source: GearSource.RAID },
+    [Slot.BODY]: { ...piece, source: GearSource.RAID },
+    [Slot.BRACELET]: { ...piece, source: GearSource.RAID },
+    [Slot.EARRINGS]: { ...piece, source: GearSource.TOME },
+    [Slot.FEET]: { ...piece, source: GearSource.RAID },
+    [Slot.HANDS]: { ...piece, source: GearSource.TOME },
+    [Slot.HEAD]: { ...piece, source: GearSource.TOME },
+    [Slot.LEGS]: { ...piece, source: GearSource.TOME },
+    [Slot.NECKLACE]: { ...piece, source: GearSource.RAID },
+    [Slot.RING1]: { ...piece, source: GearSource.TOME },
+    [Slot.RING2]: { ...piece, source: GearSource.TOME },
+    [Slot.WEAPON]: { ...piece, source: GearSource.RAID },
   },
   job: Jobs.RPR,
   name: 'New Gear Set',
   modified: true,
+  archived: false,
 }
